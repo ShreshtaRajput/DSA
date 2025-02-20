2e6ab8778626e74043afd064f3f7e6f59bc0511f
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2025-02-20 19:12:03 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (66.49%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        int string_size = s.size();
-        int part_size = part.size();
-        while(string_size >= part_size){
-            bool found = false;
-            for(int i = 0; i <= string_size - part_size; i++){
-                string temp_str = s.substr(i, part_size);
-                if(part.compare(temp_str) == 0){
-                    s.erase(i, part_size);
-                    string_size = s.size();
-                    found = true;
-                    i = -1;
-                }
-            }
-            if(!found) break;
+        // MY APPROACH
+        // int string_size = s.size();
+        // int part_size = part.size();
+        // while(string_size >= part_size){
+        //     bool found = false;
+        //     for(int i = 0; i <= string_size - part_size; i++){
+        //         string temp_str = s.substr(i, part_size);
+        //         if(part.compare(temp_str) == 0){
+        //             s.erase(i, part_size);
+        //             string_size = s.size();
+        //             found = true;
+        //             i = -1;
+        //         }
+        //     }
+        //     if(!found) break;
+        // }
+        // return s;
+
+        // OPTIMAL SOLUTION
+        while(s.length() != 0 && s.find(part) < s.length()){
+            s.erase(s.find(part), part.length());
         }
         return s;
     }
