a2df008f06198929c54a1caa8ea7bd87be43f35b
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2025-02-26 15:31:33 +0530

    Time: 26 ms (5.02%) | Memory: 14.3 MB (5.23%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        vector<int> arr;
+
+        if(x < 0){
+            return false;
+        }
+
+        while( x > 0){
+            int num = x % 10;
+            x = x/10;
+            arr.push_back(num);
+        }
+
+        vector<int> revArr = arr;
+
+        int i = 0; 
+        int j = arr.size() - 1;
+        while(i < j){
+            swap(arr[i] ,arr[j]);
+            i++;
+            j--;
+        } 
+
+        for(int i = 0; i < arr.size(); i++){
+            if(arr[i] != revArr[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
