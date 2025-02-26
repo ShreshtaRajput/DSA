79d91c137d18678721443a453f1f57579d98d02f
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2025-02-26 18:42:34 +0530

    Time: 6 ms (33.02%) | Memory: 15.4 MB (6.72%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        const char* symbol[] = {"M",  "CM", "D",  "CD", "C",  "XC", "L",
-                                "XL", "X",  "IX", "V",  "IV", "I"};
+        vector<string> M{"", "M", "MM", "MMM"};
+        vector<string> C{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        vector<string> X{"", "X",  "XX",  "XXX",  "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        vector<string> I{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
 
-        string res = "";
+        return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
 
-        for(int i = 0; i < 13; i++){
-            while( num >= values[i]){
-                res += symbol[i];
-                num -= values[i];
-            }
-        }
-        return res;
+        // OPTIMUM APPROACH
+        // int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        // const char* symbol[] = {"M",  "CM", "D",  "CD", "C",  "XC", "L",
+        //                         "XL", "X",  "IX", "V",  "IV", "I"};
+
+        // string res = "";
+
+        // for(int i = 0; i < 13; i++){
+        //     while( num >= values[i]){
+        //         res += symbol[i];
+        //         num -= values[i];
+        //     }
+        // }
+        // return res;
     }
 };
