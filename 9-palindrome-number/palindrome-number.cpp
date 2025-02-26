e52e4224b1a97bc2e7518e16783071b06b162a9f
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2025-02-26 15:42:07 +0530

    Time: 1 ms (60.61%) | Memory: 8.7 MB (26.31%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,35 +1,45 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        vector<int> arr;
+        // vector<int> arr;
+
+        // if(x < 0){
+        //     return false;
+        // }
+
+        // while( x > 0){
+        //     int num = x % 10;
+        //     x = x/10;
+        //     arr.push_back(num);
+        // }
+
+        // vector<int> revArr = arr;
+
+        // int i = 0; 
+        // int j = arr.size() - 1;
+        // while(i < j){
+        //     swap(arr[i] ,arr[j]);
+        //     i++;
+        //     j--;
+        // } 
+
+        // return (arr == revArr);
+
+        // OPTIMUM APPROACH
 
         if(x < 0){
             return false;
         }
 
-        while( x > 0){
-            int num = x % 10;
-            x = x/10;
-            arr.push_back(num);
+        long long reversed = 0;
+        long long temp = x;
+
+        while(temp != 0){
+            int digit = temp % 10;
+            reversed = reversed * 10 + digit;
+            temp = temp/10;
         }
 
-        vector<int> revArr = arr;
-
-        int i = 0; 
-        int j = arr.size() - 1;
-        while(i < j){
-            swap(arr[i] ,arr[j]);
-            i++;
-            j--;
-        } 
-
-        // for(int i = 0; i < arr.size(); i++){
-        //     if(arr[i] != revArr[i]){
-        //         return false;
-        //     }
-        // }
-        // return true;
-
-        return (arr == revArr);
+        return (reversed == x);
     }
 };
