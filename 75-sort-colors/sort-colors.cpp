8bf3efd2425388044bee58762d91853dcd18beb2
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2025-03-01 16:59:37 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (92.30%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
+    void sortColors(vector<int>& nums) {\
+        // SIMPLE SORTING
         // for(int i = 0; i < nums.size(); i++){
         //     for(int j = 0; j < nums.size() - 1 - i; j++){
         //         if(nums[j] > nums[j + 1]) {
@@ -9,35 +10,53 @@ public:
         //     }
         // }
 
-        int count0 = 0;
-        int count1 = 0;
-        int count2 = 0;
+        // ANOTHER APPROACH
+        // int count0 = 0;
+        // int count1 = 0;
+        // int count2 = 0;
 
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] == 0){
-                count0++;
-            }
-            else if(nums[i] == 1){
-                count1++;
-            }
-            else{
-                count2++;
-            }
-        }
+        // for(int i = 0; i < nums.size(); i++){
+        //     if(nums[i] == 0){
+        //         count0++;
+        //     }
+        //     else if(nums[i] == 1){
+        //         count1++;
+        //     }
+        //     else{
+        //         count2++;
+        //     }
+        // }
 
-        int i = 0;
+        // int i = 0;
 
-        while(count0--){
-            nums[i] = 0;
-            i++;
-        }
-        while(count1--){
-            nums[i] = 1;
-            i++;
-        }
-        while(count2--){
-            nums[i] = 2;
-            i++;
+        // while(count0--){
+        //     nums[i] = 0;
+        //     i++;
+        // }
+        // while(count1--){
+        //     nums[i] = 1;
+        //     i++;
+        // }
+        // while(count2--){
+        //     nums[i] = 2;
+        //     i++;
+        // }
+
+        // DUTCH NATIONAL FLAG ALGO (constant space)
+        int low = 0, mid = 0, high = nums.size() - 1;
+
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }else{
+                swap(nums[high], nums[mid]);
+                high--;
+            }
         }
     }
 };
