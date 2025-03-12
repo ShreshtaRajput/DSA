2aacf829b271729bb60d380ceb4a75c14f1331e6
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2025-03-12 17:17:54 +0530

    Time: 0 ms (100.00%) | Memory: 21.5 MB (43.55%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,16 +1,55 @@
 class Solution {
-public:
-    int maximumCount(vector<int>& nums) {
-        int posCount = 0;
-        int negCount = 0;
+private:
+    int firstZero(vector<int>& nums) {
+        int start = 0;
+        int end = nums.size() - 1;
+        int index = nums.size();
 
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] < 0){
-                negCount++;
-            }else if(nums[i] > 0){
-                posCount++;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] >= 0) {
+                end = mid - 1;
+                index = mid;
+            } else if (nums[mid] < 0) {
+                start = mid + 1;
             }
         }
+
+        return index;
+    }
+
+    int lastZero(vector<int>& nums) {
+        int start = 0;
+        int end = nums.size() - 1;
+        int index = nums.size();
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] <= 0) {
+                start = mid + 1;
+            } else if (nums[mid] > 0) {
+                end = mid - 1;
+                index = mid;
+            }
+        }
+
+        return index;
+    }
+
+public:
+    int maximumCount(vector<int>& nums) {
+        int posCount = nums.size() - lastZero(nums);
+        int negCount = firstZero(nums);
+
+        // for (int i = 0; i < nums.size(); i++) {
+        //     if (nums[i] < 0) {
+        //         negCount++;
+        //     } else if (nums[i] > 0) {
+        //         posCount++;
+        //     }
+        // }
 
         int ans = max(posCount, negCount);
         return ans;
