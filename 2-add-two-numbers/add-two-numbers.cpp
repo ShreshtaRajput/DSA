6f151ac1514e6cca33d1d8831b311a9a489e4c57
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2025-05-06 20:50:55 +0530

    Time: 7 ms (7.86%) | Memory: 77.3 MB (12.17%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -30,38 +30,63 @@ public:
         ListNode* ansHead = NULL;
         ListNode* ansTail = NULL;
 
-        while(l1 != NULL && l2 != NULL){
-            int sum = carry + l1 -> val + l2 -> val;
-            int digit = sum%10;
-            insertAtTail(ansHead, ansTail, digit);
-            carry = sum/10;
-            l1 = l1 -> next;
-            l2 = l2 -> next;
-        }
+        while(l1 != NULL || l2 != NULL || carry != 0){
+            int val1 = 0;
+            if(l1 != NULL){
+                val1 = l1 -> val;
+            }
 
-        while(l1 != NULL){
-            int sum = carry + l1 -> val;
-            int digit = sum%10;
-            insertAtTail(ansHead, ansTail, digit);
-            carry = sum/10;
-            l1 = l1 -> next;
-        }
+            int val2 = 0;
+            if(l2 != NULL){
+                val2 = l2 -> val;
+            }
 
-        while(l2 != NULL){
-            int sum = carry + l2 -> val;
+            int sum = carry + val1 + val2;
             int digit = sum%10;
             insertAtTail(ansHead, ansTail, digit);
             carry = sum/10;
-            l2 = l2 -> next;
-        }
 
-        while(carry != 0){
-            int sum = carry;
-            int digit = sum%10;
-            insertAtTail(ansHead, ansTail, digit);
-            carry = sum/10;
+            if(l1 != NULL){
+                l1 = l1 -> next;
+            }
+
+            if(l2 != NULL){
+                l2 = l2 -> next;    
+            }
         }
 
         return ansHead;
     }
 };
+
+// while(l1 != NULL && l2 != NULL){
+//     int sum = carry + l1 -> val + l2 -> val;
+//     int digit = sum%10;
+//     insertAtTail(ansHead, ansTail, digit);
+//     carry = sum/10;
+//     l1 = l1 -> next;
+//     l2 = l2 -> next;
+// }
+
+// while(l1 != NULL){
+//     int sum = carry + l1 -> val;
+//     int digit = sum%10;
+//     insertAtTail(ansHead, ansTail, digit);
+//     carry = sum/10;
+//     l1 = l1 -> next;
+// }
+
+// while(l2 != NULL){
+//     int sum = carry + l2 -> val;
+//     int digit = sum%10;
+//     insertAtTail(ansHead, ansTail, digit);
+//     carry = sum/10;
+//     l2 = l2 -> next;
+// }
+
+// while(carry != 0){
+//     int sum = carry;
+//     int digit = sum%10;
+//     insertAtTail(ansHead, ansTail, digit);
+//     carry = sum/10;
+// }
