fbb97fe16d294311dcaf3dc29257d64e737c1b86
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2025-06-30 00:33:28 +0530

    Time: 0 ms (100.00%) | Memory: 60.7 MB (96.34%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -6,40 +6,67 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 private:
-    void getInorder(TreeNode* root, vector<int> &ans){
-        if(root == NULL){
+    void getInorder(TreeNode* root, vector<int>& ans) {
+        if (root == NULL) {
             return;
         }
 
-        getInorder(root -> left, ans);
-        ans.push_back(root -> val);
-        getInorder(root -> right, ans);
+        getInorder(root->left, ans);
+        ans.push_back(root->val);
+        getInorder(root->right, ans);
     }
 
     // Inorder again to updates nodes in sorted array
-    void inorderAgain(TreeNode* &root, vector<int> &in, int &index){
-        if(root == NULL){
+    void inorderAgain(TreeNode*& root, vector<int>& in, int& index) {
+        if (root == NULL) {
             return;
         }
 
-        inorderAgain(root -> left, in, index);
-        root -> val = in[index];
+        inorderAgain(root->left, in, index);
+        root->val = in[index];
         index++;
-        inorderAgain(root -> right, in, index);
+        inorderAgain(root->right, in, index);
     }
 
+    void inorder(TreeNode*& root, TreeNode*& first, TreeNode*& middle, TreeNode*& prev) {
+        if (root == NULL) {
+            return;
+        }
+
+        inorder(root->left, first, middle, prev);
+        if (prev != NULL && root->val < prev->val) {
+            if (first == NULL) {
+                first = prev;
+                middle = root;
+            } else {
+                middle = root;
+            }
+        }
+        prev = root;
+
+        inorder(root->right, first, middle, prev);
+    }
 
 public:
     void recoverTree(TreeNode* root) {
-        vector<int> in;
-        getInorder(root, in);
-        sort(in.begin(), in.end());
-        int index = 0;
-        inorderAgain(root, in, index);
+        // vector<int> in;
+        // getInorder(root, in);
+        // sort(in.begin(), in.end());
+        // int index = 0;
+        // inorderAgain(root, in, index);
+
+        TreeNode* first = NULL;
+        TreeNode* middle = NULL;
+        TreeNode* prev = NULL;
+        inorder(root, first, middle, prev);
+        if(first && middle){
+            swap(first -> val, middle -> val);
+        }
     }
 };
