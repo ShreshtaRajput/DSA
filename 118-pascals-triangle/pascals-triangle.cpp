d5390f08c4c07f7cf6a35ce18ecede9757e2d35c
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2025-08-01 14:59:29 +0530

    Time: 1 ms (19.21%) | Memory: 9.8 MB (30.63%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -2,10 +2,12 @@ class Solution {
 private:
     // Calculate the combination
     long long nCr(int n, int r) {
-        if (r > n)
+        if (r > n){
             return 0;
-        if (r > n - r)
+        }
+        if (r > n - r){
             r = n - r;
+        }
         long long res = 1;
         for (int i = 0; i < r; i++) {
             res *= (n - i);
