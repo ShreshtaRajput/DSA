e9a87014b6e17f4f96d1b3b2c0e42701f86af4ee
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2025-08-13 20:15:05 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (86.91%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,22 +1,53 @@
 class Solution {
 private:
-    void solve(vector<int> nums, vector<vector<int>>& ans, int i){
-        // Base case
-        if(i >= nums.size()){
-            ans.push_back(nums);
+    void helper(vector<int> &nums, vector<vector<int>> &ans, vector<int> &temp, vector<int> &freq, int n){
+        if(temp.size() >= n){
+            ans.push_back(temp);
             return;
         }
 
-        for(int j = i; j < nums.size(); j++){
-            swap(nums[i], nums[j]);
-            solve(nums, ans, i+1);
+        for(int i = 0; i < n; i++){
+            if(!freq[i]){
+                freq[i] = 1;
+                temp.push_back(nums[i]);
+                helper(nums, ans, temp, freq, n);
+                temp.pop_back();
+                freq[i] = 0;
+            }
         }
     }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
+        int n = nums.size();
         vector<vector<int>> ans;
-        int index = 0;
-        solve(nums, ans, index);
+        vector<int> temp;
+        vector<int> freq(n,0);
+
+        helper(nums, ans, temp, freq, n);
+
         return ans;
     }
 };
+
+// class Solution {
+// private:
+//     void solve(vector<int> nums, vector<vector<int>>& ans, int i){
+//         // Base case
+//         if(i >= nums.size()){
+//             ans.push_back(nums);
+//             return;
+//         }
+
+//         for(int j = i; j < nums.size(); j++){
+//             swap(nums[i], nums[j]);
+//             solve(nums, ans, i+1);
+//         }
+//     }
+// public:
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vector<vector<int>> ans;
+//         int index = 0;
+//         solve(nums, ans, index);
+//         return ans;
+//     }
+// };
