0245fe7462aef884e51f6ec6b42e5c9291d88171
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2025-08-28 01:46:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (22.70%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -2,7 +2,7 @@ class Solution {
 private:
     int solve(vector<int> &nums, vector<int> &dp, int index){
         // Base case => if we reach at index 0, it means that we have not picked index 1
-        // so we necessarily need to pick the index 1
+        // so we necessarily need to pick the index 0
         if(index == 0){
             return nums[index];
         }
@@ -25,6 +25,20 @@ public:
         int n = nums.size();
         vector<int> dp(n+1, -1);
 
-        return solve(nums, dp, n-1);
+        // TABULATION
+        dp[0] = nums[0];
+
+        int pick = 0;
+        int notPick = 0;
+        for(int i = 1; i < n; i++){
+            pick = nums[i];
+            if(i > 1) pick += dp[i-2];
+            notPick = dp[i-1];
+
+            dp[i] = max(pick, notPick);
+        }
+
+        return dp[n-1];
+        // return solve(nums, dp, n-1);
     }
 };
