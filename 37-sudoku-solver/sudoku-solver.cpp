e8d4c5a9b2d05e4185b9ade6bd44602908b76f00
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2025-08-31 22:04:38 +0530

    Time: 422 ms (31.23%) | Memory: 8.6 MB (87.81%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,31 +1,31 @@
 class Solution {
 private:
-    bool isSafe(int row, int col, vector<vector<char>>& board, char val){
+    bool isValid(int row, int col, vector<vector<char>>& board, char val){
         for(int i = 0; i < board.size(); i++){
             // row check
-            if( board[row][i] == val ){
+            if(board[row][i] == val){
                 return false;
             }
-            // column check
-            if( board[i][col] == val ){
+
+            // col check
+            if(board[i][col] == val){
                 return false;
             }
-            // Sub matrix check
-            if( board[3*(row/3) + i/3][3*(col/3) + i%3] == val){
+
+            // sub-matrix check
+            if(board[3*(row/3) + i/3][3*(col/3) + i%3] == val){
                 return false;
             }
         }
-
         return true;
     }
-
     bool solve(vector<vector<char>>& board){
         for(int row = 0; row < 9; row++){
             for(int col = 0; col < 9; col++){
                 if(board[row][col] == '.'){
-                    for(int val = '1'; val <= '9'; val++){
-                        if(isSafe(row, col, board, val)){
-                            board[row][col] = val;
+                    for(char i = '1'; i <= '9'; i++){
+                        if(isValid(row, col, board, i)){
+                            board[row][col] = i;
 
                             if(solve(board)){
                                 return true;
@@ -41,7 +41,6 @@ private:
         }
         return true;
     }
-
 public:
     void solveSudoku(vector<vector<char>>& board) {
         solve(board);
