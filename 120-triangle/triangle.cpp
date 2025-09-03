93a826a7fa70f3c679c2f95734ce255ac44f02da
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2025-09-03 16:54:26 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (82.77%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -18,24 +18,43 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
 
-        vector<vector<int>> dp(n, vector<int>(n, -1));
+        // vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        vector<int> front(n, 0), curr(n, 0);
+
+        for(int j = 0; j < n; j++){
+            front[j] = triangle[n-1][j];
+        }
+
+        for(int i = n-2; i >= 0; i--){
+            for(int j = i; j >= 0; j--){
+                int way1 = triangle[i][j] + front[j];
+                int way2 = triangle[i][j] + front[j+1];
+
+                curr[j] = min(way1, way2);
+            }
+
+            front = curr;
+        }
+
+        return front[0];
 
         // TABULATION - since the memoization is top-down
         // the tabulation has to be it's opposite
-        for(int j = 0; j < n; j++){
-            dp[n-1][j] = triangle[n-1][j];
-        }
+        // for(int j = 0; j < n; j++){
+        //     dp[n-1][j] = triangle[n-1][j];
+        // }
         
-        for(int i = n-2; i >= 0; i--){
-            for(int j = triangle[i].size() - 1; j >= 0; j--){
-                int way1 = triangle[i][j] + dp[i+1][j];
-                int way2 = triangle[i][j] + dp[i+1][j+1];
+        // for(int i = n-2; i >= 0; i--){
+        //     for(int j = triangle[i].size() - 1; j >= 0; j--){
+        //         int way1 = triangle[i][j] + dp[i+1][j];
+        //         int way2 = triangle[i][j] + dp[i+1][j+1];
 
-                dp[i][j] = min(way1, way2);
-            }
-        }
+        //         dp[i][j] = min(way1, way2);
+        //     }
+        // }
 
-        return dp[0][0];
+        // return dp[0][0];
         // return solve(0, 0, triangle, dp);
     }
 };
