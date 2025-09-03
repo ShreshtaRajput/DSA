c5998f6ab32b0857eb97714665bc97881ecd5ef1
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2025-09-03 16:40:06 +0530

    Time: 7 ms (4.22%) | Memory: 13.4 MB (33.33%) - LeetSync

---
+++ 120-triangle/triangle.cpp
@@ -0,0 +1,41 @@
+class Solution {
+private:
+    int solve(int row, int col, vector<vector<int>> &triangle, vector<vector<int>> &dp){
+        int n = triangle.size();
+        // Base case
+        if(row == n-1){
+            return triangle[row][col];
+        }
+
+        if(dp[row][col] != -1) return dp[row][col];
+
+        int way1 = triangle[row][col] + solve(row + 1, col, triangle, dp);
+        int way2 = triangle[row][col] + solve(row + 1, col + 1, triangle, dp);
+
+        return dp[row][col] = min(way1, way2);
+    }
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        // TABULATION - since the memoization is top-down
+        // the tabulation has to be it's opposite
+        for(int j = 0; j < n; j++){
+            dp[n-1][j] = triangle[n-1][j];
+        }
+        
+        for(int i = n-2; i >= 0; i--){
+            for(int j = triangle[i].size() - 1; j >= 0; j--){
+                int way1 = triangle[i][j] + dp[i+1][j];
+                int way2 = triangle[i][j] + dp[i+1][j+1];
+
+                dp[i][j] = min(way1, way2);
+            }
+        }
+
+        return dp[0][0];
+        // return solve(0, 0, triangle, dp);
+    }
+};
