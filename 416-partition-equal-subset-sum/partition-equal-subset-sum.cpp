1117d8c4915519e3a0e906b7bcc836bb523a3fab
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2025-09-10 23:55:06 +0530

    Time: 147 ms (40.05%) | Memory: 17.5 MB (55.67%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,39 +1,62 @@
 class Solution {
 private:
-    bool solve(vector<int> &nums, int index, int target, vector<vector<int>> &dp){
+    bool solve(vector<int>& nums, int index, int target,
+               vector<vector<int>>& dp) {
         // Base case
-        if(target == 0) return true;
-        if(index == 0) return (target == nums[0]);
+        if (target == 0)
+            return true;
+        if (index == 0)
+            return (target == nums[0]);
 
-        if(dp[index][target] != -1) return dp[index][target];
+        if (dp[index][target] != -1)
+            return dp[index][target];
 
         bool notTake = solve(nums, index - 1, target, dp);
         bool take = false;
-        if(target >= nums[index]){
+        if (target >= nums[index]) {
             take = solve(nums, index - 1, target - nums[index], dp);
         }
 
         return dp[index][target] = notTake || take;
     }
+
 public:
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
 
         int totSum = 0;
-        for(auto it: nums) totSum += it;
+        for (auto it : nums)
+            totSum += it;
 
-        if(totSum % 2 != 0) return false;
+        if (totSum % 2 != 0)
+            return false;
 
-        int target = totSum/2;
+        int target = totSum / 2;
 
-        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
+        vector<vector<bool>> dp(n, vector<bool>(target + 1, false));
 
         // TABULATION
         // For every index where target = 0, we will return true
-        // for(int i = 0; i < n; i++){
-        //     dp[i][0] = 1;
-        // }
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+        
+        if (nums[0] <= target)
+            dp[0][nums[0]] = true;
 
-        return solve(nums, n-1, target, dp);
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= target; j++) {
+                bool notTake = dp[i-1][j];
+                bool take = false;
+                if (j >= nums[i]) {
+                    take = dp[i-1][j-nums[i]];
+                }
+
+                dp[i][j] = take || notTake;
+            }
+        }
+
+        return dp[n-1][target];
+        // return solve(nums, n - 1, target, dp);
     }
 };
