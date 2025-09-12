e07d635e34eb0b01b18c6de5559d48992805489f
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2025-09-13 01:54:32 +0530

    Time: 3 ms (59.74%) | Memory: 17.3 MB (47.77%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -1,39 +1,44 @@
 class Solution {
 private:
-    int solve(vector<int> &unique, unordered_map<int, int> &points, int index, vector<int> &dp){
+    int solve(vector<int>& unique, unordered_map<int, int>& points, int index,
+              vector<int>& dp) {
         // BASE CASE
-        if(index < 0){
+        if (index < 0) {
             return 0;
         }
 
-        if(index == 0) return points[unique[0]] * unique[0];
+        if (index == 0)
+            return points[unique[0]] * unique[0];
 
-        if(dp[index] != -1) return dp[index];
+        if (dp[index] != -1)
+            return dp[index];
 
         int skip = solve(unique, points, index - 1, dp);
         int take = unique[index] * points[unique[index]];
-        if(unique[index] == unique[index - 1] - 1 || unique[index] == unique[index - 1] + 1){
-            take += solve(unique, points, index - 2, dp);   //skip the previous numebr
-        }else{
-            take += solve(unique, points, index - 1, dp);   //Take the number
+        if (unique[index] == unique[index - 1] - 1 ||
+            unique[index] == unique[index - 1] + 1) {
+            take += solve(unique, points, index - 2,
+                          dp); // skip the previous numebr
+        } else {
+            take += solve(unique, points, index - 1, dp); // Take the number
         }
-
 
         return dp[index] = max(take, skip);
     }
+
 public:
     int deleteAndEarn(vector<int>& nums) {
         // first we will need an array with all the unique elements
         // We can store all the points in a map
         unordered_map<int, int> points;
 
-        for(auto it: nums){
+        for (auto it : nums) {
             points[it]++;
         }
 
         // Create a vector with all the unique items
         vector<int> unique;
-        for(auto p: points){
+        for (auto p : points) {
             unique.push_back(p.first);
         }
         sort(unique.begin(), unique.end());
@@ -42,6 +47,28 @@ public:
 
         vector<int> dp(n, -1);
 
-        return solve(unique, points, n-1, dp);
+        // TABULATION
+        dp[0] = points[unique[0]] * unique[0];
+
+        for (int i = 1; i < n; i++) {
+            int skip = dp[i - 1];
+            int take = unique[i] * points[unique[i]];
+            if ((unique[i] == unique[i - 1] - 1) ||
+                (unique[i] == unique[i - 1] + 1)) {
+                if (i >= 2) {
+                    take += dp[i - 2]; // skip the previous number
+                } else {
+                    take += 0;
+                }
+            } else {
+                take += dp[i - 1]; // Take the number
+            }
+
+            dp[i] = max(take, skip);
+        }
+
+        return dp[n - 1];
+
+        // return solve(unique, points, n - 1, dp);
     }
 };
