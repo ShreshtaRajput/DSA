92bded590e2a3b724edf4f7f8700fd8d3c57ab7e
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2025-09-24 00:44:45 +0530

    Time: 5 ms (72.42%) | Memory: 13.1 MB (49.03%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -15,7 +15,7 @@ private:
 
         // If the chars match
         if (word1[i] == word2[j])
-            return solve(word1, word2, i - 1, j - 1, dp);
+            return dp[i][j] = solve(word1, word2, i - 1, j - 1, dp);
 
         // f(i-1, j) -> deletion, f(i, j-1) -> insertion, f(i-1, j-1) ->
         // replacement
@@ -29,8 +29,23 @@ public:
         int m = word1.size();
         int n = word2.size();
 
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
 
-        return solve(word1, word2, m-1, n-1, dp);
+        // TABULATE
+        for(int i = 0; i <= m; i++) dp[i][0] = i;
+        for(int j = 0; j <= n; j++) dp[0][j] = j;
+
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    dp[i][j] = 1 + min(dp[i-1][j], min(dp[i-1][j-1], dp[i][j-1]));
+                }
+            }
+        }
+
+        return dp[m][n];
+        // return solve(word1, word2, m-1, n-1, dp);
     }
 };
