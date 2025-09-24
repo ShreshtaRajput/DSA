be838ec4b6812e8f443d022c734808c100f21e67
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2025-09-24 23:00:50 +0530

    Time: 4 ms (32.53%) | Memory: 22.2 MB (14.26%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+private:
+    int solve(vector<int>& prices, int index, int status, vector<vector<int>> &dp) {
+        int n = prices.size();
+        // Base case
+        if (index == n)
+            return 0;
+
+        int profit = 0;
+
+        if(dp[index][status] != -1) return dp[index][status];
+
+        // If status is 1 -> we can buy else we sell
+        if (status == 1) {
+            // Whenever we buy a stock, we subtract it's value from the profit
+            // Here we can either buy a stock on that index, or we can move forward without buying
+            profit = max(-prices[index] + solve(prices, index + 1, 0, dp),
+                         solve(prices, index + 1, 1, dp));
+        } else {
+            // Whenever we sell a stock we add it's value to the profit
+            // Here we can either sell a stock on that index, or we can move forward without selling
+            profit = max(prices[index] + solve(prices, index + 1, 1, dp),
+                         solve(prices, index + 1, 0, dp));
+        }
+
+        return dp[index][status] = profit;
+    }
+
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+
+        return solve(prices, 0, 1, dp); 
+    }
+};
