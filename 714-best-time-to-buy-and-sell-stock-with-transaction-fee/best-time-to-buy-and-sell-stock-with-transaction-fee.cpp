71386d7507e0aaf1fce54ad4bc005717e206b605
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2025-09-27 04:50:52 +0530

    Time: 117 ms (21.79%) | Memory: 100.4 MB (12.56%) - LeetSync

---
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+    int solve(vector<int> &prices, int fee, int index, int status, vector<vector<int>> &dp){
+        int n = prices.size();
+        // Base case
+        if(index == n) return 0;
+
+        int profit = 0;
+
+        if(dp[index][status] != -1) return dp[index][status];
+
+        if(status){
+            profit = max(-prices[index] + solve(prices, fee, index + 1, 0, dp),
+                            solve(prices, fee, index + 1, 1, dp));
+        }else{
+            profit = max(prices[index] - fee + solve(prices, fee, index + 1, 1, dp),
+                            solve(prices, fee, index + 1, 0, dp));
+        }
+
+        return dp[index][status] = profit;
+    }
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int n = prices.size();
+
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+
+        return solve(prices, fee, 0, 1, dp);
+    }
+};
