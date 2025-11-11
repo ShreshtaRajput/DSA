dc6c77842928c74f10dc23ff1a24393b4badd460
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2025-11-12 02:27:04 +0530

    Time: 51 ms (81.11%) | Memory: 13.8 MB (83.82%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/ones-and-zeroes.cpp
@@ -25,7 +25,26 @@ private:
     }
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
-        vector<vector<vector< int>>> dp(strs.size(), vector<vector<int>>(m+1, vector<int>(n+1, -1)));
-        return solve(strs, 0, m, n, dp);
+        // vector<vector<vector< int>>> dp(strs.size(), vector<vector<int>>(m+1, vector<int>(n+1, -1)));
+        // return solve(strs, 0, m, n, dp);
+
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        for (string &s : strs) {
+            int numZeroes = 0, numOnes = 0;
+            for (char c : s) {
+                if (c == '0') numZeroes++;
+                else numOnes++;
+            }
+
+            // Traverse backward to prevent using the same string multiple times
+            for (int i = m; i >= numZeroes; i--) {
+                for (int j = n; j >= numOnes; j--) {
+                    dp[i][j] = max(dp[i][j], 1 + dp[i - numZeroes][j - numOnes]);
+                }
+            }
+        }
+
+        return dp[m][n];
     }
 };
