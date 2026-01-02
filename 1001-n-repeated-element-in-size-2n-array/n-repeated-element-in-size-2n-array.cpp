b43be933abad8d907b6dcfafd20329f15b535b95
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2026-01-02 20:10:23 +0530

    Time: 0 ms (100.00%) | Memory: 28.4 MB (69.74%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
-        int len = nums.size();
-        int n = len/2;
-
-        unordered_map<int, int> mp;
-        for(auto &num: nums){
-            mp[num]++;
-        }
-
-        for(auto &p: mp){
-            if(p.second == n){
-                return p.first;
+        for(int i = 0; i < nums.size() - 2; i++){
+            // If the element is repeated n time for an array of size 2*n, then the element must be present
+            // after every 1 or 2 elements
+            if(nums[i] == nums[i+1] || nums[i] == nums[i+2]){
+                return nums[i];
             }
         }
 
-        return 0;
+        return nums[nums.size() - 1];
+        // int len = nums.size();
+        // int n = len/2;
+
+        // unordered_map<int, int> mp;
+        // for(auto &num: nums){
+        //     mp[num]++;
+        // }
+
+        // for(auto &p: mp){
+        //     if(p.second == n){
+        //         return p.first;
+        //     }
+        // }
+
+        // return 0;
     }
 };
