9e2f2246f714236c6af23383fe6dbc4ee20f841a
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2026-01-10 21:44:27 +0530

    Time: 15 ms (74.15%) | Memory: 18.1 MB (67.29%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -41,8 +41,29 @@ public:
         int n1 = s1.size();
         int n2 = s2.size();
 
-        vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0));
 
-        return solve(n1 - 1, n2 - 1, s1, s2, dp);
+        // Delete all the values from s1
+        for(int i = 1; i <= n1; i++){
+            dp[i][0] = dp[i-1][0] + s1[i-1];
+        }
+
+        // Delete all the values from s2
+        for(int i = 1; i <= n2; i++){
+            dp[0][i] = dp[0][i-1] + s2[i-1];
+        }
+
+        for(int i = 1; i <= n1; i++){
+            for(int j = 1; j <= n2; j++){
+                if(s1[i-1] == s2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    dp[i][j] = min(s1[i-1] + dp[i-1][j], s2[j-1] + dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[n1][n2];
+        // return solve(n1 - 1, n2 - 1, s1, s2, dp);
     }
 };
