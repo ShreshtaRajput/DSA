4d001629f4d6601cb2796eb706050f5b081c1736
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2026-01-19 17:16:39 +0530

    Time: 12 ms (65.45%) | Memory: 32.8 MB (60.91%) - LeetSync

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -1,8 +1,8 @@
 class Solution {
-private:
-    int getSum(int r, int c, int k, vector<vector<int>> &ps){
-        return ps[r+k-1][c+k-1] - ps[r-1][c+k-1] - ps[r+k-1][c-1] + ps[r-1][c-1];
-    }
+// private:
+//     int getSum(int r, int c, int k, vector<vector<int>> &ps){
+//         return ps[r+k-1][c+k-1] - ps[r-1][c+k-1] - ps[r+k-1][c-1] + ps[r-1][c-1];
+//     }
 public:
     int maxSideLength(vector<vector<int>>& mat, int threshold) {
         int n = mat.size();
@@ -19,19 +19,47 @@ public:
 
         int res = 0;
 
-        // Check for a kxk matrix
-        for(int k = 1; k <= min(n, m); k++){
-            int sum = 0;
-            for(int i = 1; i <= n - k + 1; i++){
-                for(int j = 1; j <= m - k + 1; j++){
-                    sum = getSum(i, j, k, ps);
+        // Binary search optimization
+        int low = 0;
+        int high = min(n, m);
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            bool valid = false;
+
+            for(int i = mid; i <= n; i++){
+                for(int j = mid; j <= m; j++){
+                    int sum = ps[i][j] - ps[i-mid][j] - ps[i][j-mid] + ps[i-mid][j-mid];
 
                     if(sum <= threshold){
-                        res = k;
+                        valid = true;
+                        break;
                     }
                 }
+                if(valid) break;
+            }
+
+            if(valid){
+                res = mid;
+                low = mid + 1;
+            }else{
+                high = mid - 1;
             }
         }
+        
+        // Check for a kxk matrix
+        // for(int k = 1; k <= min(n, m); k++){
+        //     int sum = 0;
+        //     for(int i = 1; i <= n - k + 1; i++){
+        //         for(int j = 1; j <= m - k + 1; j++){
+        //             sum = getSum(i, j, k, ps);
+
+        //             if(sum <= threshold){
+        //                 res = k;
+        //             }
+        //         }
+        //     }
+        // }
 
         return res;
     }
