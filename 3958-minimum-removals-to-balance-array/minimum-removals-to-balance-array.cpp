3d33ca2e7fd0da244fdb72f11360938b891cd9c7
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2026-02-06 14:15:16 +0530

    Time: 43 ms (26.74%) | Memory: 105.1 MB (14.24%) - LeetSync

--- 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.cpp
+++ 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.cpp
@@ -4,28 +4,41 @@ public:
         sort(nums.begin(), nums.end());
         int n = nums.size();
 
-        int maxLen = 0;
+        int ans = n;
+        int right = 0;
 
-        for(int i = 0; i < n; i++){
-            long long limit = (long long)nums[i]*k;
-
-            int s = i;
-            int e = n - 1;
-            int idx = i;
-
-            while(s <= e){
-                int mid = s + (e-s)/2;
-                if(nums[mid] <= limit){
-                    idx = mid;
-                    s = mid + 1;
-                }else{
-                    e = mid - 1;
-                }
+        for(int left = 0; left < n; left++){
+            long long target = (long long)nums[left]*k;
+            while(right < n && nums[right] <= target){
+                right++;
             }
-
-            maxLen = max(maxLen, idx - i + 1);
+            ans = min(ans, n - (right - left));
         }
 
-        return n - maxLen;
+        return ans;
+
+        // int maxLen = 0;
+
+        // for(int i = 0; i < n; i++){
+        //     long long limit = (long long)nums[i]*k;
+
+        //     int s = i;
+        //     int e = n - 1;
+        //     int idx = i;
+
+        //     while(s <= e){
+        //         int mid = s + (e-s)/2;
+        //         if(nums[mid] <= limit){
+        //             idx = mid;
+        //             s = mid + 1;
+        //         }else{
+        //             e = mid - 1;
+        //         }
+        //     }
+
+        //     maxLen = max(maxLen, idx - i + 1);
+        // }
+
+        // return n - maxLen;
     }
 };
