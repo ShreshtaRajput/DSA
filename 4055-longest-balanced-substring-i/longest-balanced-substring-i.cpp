c3248dc341d91af83f16d259d37792629268097c
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2026-02-12 20:03:41 +0530

    Time: 47 ms (96.24%) | Memory: 25.4 MB (52.24%) - LeetSync

--- 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
@@ -2,24 +2,26 @@ class Solution {
 public:
     int longestBalanced(string s) {
         int maxLen = INT_MIN;
-        
-        for(int i = 0; i < s.size(); i++){
-            unordered_map<char, int> mp;
-            for(int j = i; j < s.size(); j++){
-                mp[s[j]]++;
+        int n = s.size();
 
-                int freq = mp[s[j]];
-                bool flag = true;
+        for(int i = 0; i < n; i++){
+            vector<int> freq(26);
+            int distinct = 0;
+            int maxFreq = 0;
 
-                for(auto p: mp){
-                    if(freq != p.second){
-                        // Frequency not equal
-                        flag = false;
-                    }
+            for(int j = i; j < n; j++){
+                int index = s[j] - 'a';
+                
+                if(freq[index] == 0){   //New element has appeared
+                    distinct++;
                 }
 
-                if(flag){
-                    maxLen = max(maxLen, j-i+1);
+                freq[index]++;
+                maxFreq = max(maxFreq, freq[index]);
+
+                int len = j-i+1;
+                if(len == distinct * maxFreq){
+                    maxLen = max(maxLen, len);
                 }
             }
         }
@@ -27,3 +29,33 @@ public:
         return maxLen;
     }
 };
+
+// class Solution {
+// public:
+//     int longestBalanced(string s) {
+//         int maxLen = INT_MIN;
+        
+//         for(int i = 0; i < s.size(); i++){
+//             unordered_map<char, int> mp;
+//             for(int j = i; j < s.size(); j++){
+//                 mp[s[j]]++;
+
+//                 int freq = mp[s[j]];
+//                 bool flag = true;
+
+//                 for(auto p: mp){
+//                     if(freq != p.second){
+//                         // Frequency not equal
+//                         flag = false;
+//                     }
+//                 }
+
+//                 if(flag){
+//                     maxLen = max(maxLen, j-i+1);
+//                 }
+//             }
+//         }
+
+//         return maxLen;
+//     }
+// };
