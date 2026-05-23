6b11cfb6d25ffdb1285f5b067702f0c7f2d18c62
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2026-05-23 16:17:49 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (53.59%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,27 +1,43 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        vector<int> nums2(nums);
+        int count = 0;
+        int n = nums.size();
 
-        for(int i = 0 ;i < nums.size(); i++){
-            nums2[i] = nums[i];
-        }
-
-        sort(nums.begin(),nums.end());
-
-        for(int rotation = 0; rotation < nums.size(); rotation++) {
-            bool isMatch = true;
-            
-            for(int i = 0; i < nums.size(); i++) {
-                if(nums[i] != nums2[(i + rotation) % nums.size()]) {
-                    isMatch = false;
-                    break;
-                }
+        for(int i = 0; i < n; i++){
+            if(nums[i] > nums[(i+1) % n]){
+                count++;
             }
-            
-            if(isMatch) return true;
         }
 
-        return false;
+        return count <= 1;
     }
 };
+
+// class Solution {
+// public:
+//     bool check(vector<int>& nums) {
+//         vector<int> nums2(nums);
+
+//         for(int i = 0 ;i < nums.size(); i++){
+//             nums2[i] = nums[i];
+//         }
+
+//         sort(nums.begin(),nums.end());
+
+//         for(int rotation = 0; rotation < nums.size(); rotation++) {
+//             bool isMatch = true;
+            
+//             for(int i = 0; i < nums.size(); i++) {
+//                 if(nums[i] != nums2[(i + rotation) % nums.size()]) {
+//                     isMatch = false;
+//                     break;
+//                 }
+//             }
+            
+//             if(isMatch) return true;
+//         }
+
+//         return false;
+//     }
+// };
