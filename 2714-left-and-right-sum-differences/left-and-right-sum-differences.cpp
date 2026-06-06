c86004db6b1e8926ec6c4c3ac84e487508c2677c
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2026-06-06 13:24:58 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (46.07%) - LeetSync

--- 2714-left-and-right-sum-differences/left-and-right-sum-differences.cpp
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.cpp
@@ -4,16 +4,12 @@ public:
         int n = nums.size();
         vector<int> ans(n), left(n), right(n);
 
-        // Build left arr
         left[0] = nums[0];
+        right[n-1] = nums[n-1];
         for(int i = 1; i < n; i++){
             left[i] = left[i-1] + nums[i];
-        }
-
-        // Buil right arr
-        right[n-1] = nums[n-1];
-        for(int i = n-2; i >= 0; i--){
-            right[i] = right[i+1] + nums[i];
+            int j = n - 1 - i;
+            right[j] = right[j + 1] + nums[j];
         }
 
         for(int i = 0; i < n; i++){
