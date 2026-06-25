7cf8ef97a8cf53cc0247a3906273befe57834979
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2026-06-26 00:02:10 +0530

    Time: 2542 ms (5.44%) | Memory: 494.2 MB (5.23%) - LeetSync

---
+++ 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countMajoritySubarrays(vector<int>& nums, int target) {
+        int res = 0;
+        int n = nums.size();
+
+        for(int i = 0; i < n; i++){
+            unordered_map<int, int> mp;
+            for(int j = i; j < n; j++){
+                mp[nums[j]]++;
+
+                if(2 * mp[target] > j-i+1){
+                    res++;
+                }
+            }
+        }
+
+        return res;
+    }
+};
