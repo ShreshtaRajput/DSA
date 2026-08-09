16ba3013b6b5c45e06ee63b66c1fd0e32ee56e64
Shreshta Rajput <136837921+ShreshtaRajput@users.noreply.github.com>
2026-08-10 02:32:30 +0530

    Time: 17 ms (31.07%) | Memory: 13.7 MB (43.69%) - LeetSync

---
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+private:
+    int solve(int index, int m, vector<int> &piles, vector<vector<int>> &dp){
+        int n = piles.size();
+
+        if(index >= n){
+            return 0;
+        }
+
+        if(dp[index][m] != -1){
+            return dp[index][m];
+        }
+
+        int ans = 0;
+        int stones = 0;
+        for(int i = 1; i <= 2*m && index + i <= n; i++){
+            stones += piles[index + i - 1];
+
+            int totalRemaining = 0;
+            for(int j = index; j < n; j++){
+                totalRemaining += piles[j];
+            }
+
+            int opp = solve(index + i, max(m, i), piles, dp);
+            ans = max(ans, totalRemaining - opp);
+        }
+
+        return dp[index][m] = ans;
+    }
+public:
+    int stoneGameII(vector<int>& piles) {
+        int n = piles.size();
+        vector<vector<int>> dp(n, vector<int>(n+1, -1));
+        return solve(0, 1, piles, dp);
+    }
+};
